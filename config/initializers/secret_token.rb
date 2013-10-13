5051ed4f6f85ea5c49d5562abfb5ba0171b2e000
Avinash Gosavi <aag1091@gmail.com>
2013-10-13 21:58:12 +0530

    added new rails add with post scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoaDemo::Application.config.secret_key_base = '0ca713a665a95ac0d8095b19bc372b0054553a2b14524a4836e36983b21a5fa891ab7e0f6ec2f29c78e8131bf86c8810fcf9ad6cb585263d1b2122f4514adcf5'
